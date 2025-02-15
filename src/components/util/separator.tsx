4d66f0199ff0fca977c8041c1343538cf412f74d
Moser José <mosmmy.jose@gmail.com>
2025-02-15 16:28:54 +0100

    many modifactions
    many modifactions
    
    Co-Authored-By: Mateus Kriste <199351435+mkistre@users.noreply.github.com>

--- src/components/util/separator.tsx
+++ src/components/util/separator.tsx
@@ -1,13 +1,32 @@
-import { View,Text } from "react-native"
-import { defaultStyles } from "@/styles"
+import { View,Text, StyleSheet, TouchableOpacity } from "react-native"
+import { fontSize } from "@/constants/styles"
 
-const Separator = ({title,more}:{title:string,more?:string})=>{
+const Separator = ({title,more}:{title:string,more?:boolean})=>{
     return (
-        <View style={defaultStyles.container}>
-            <Text style={defaultStyles.text}>{title}</Text>
-            {more && <Text style={defaultStyles.text}>{more}</Text>}
+        <View style={styles.container}>
+            <Text style={styles.title}>{title}</Text>
+            {more && <TouchableOpacity activeOpacity={0.8}><Text style={styles.more}>Ver mais</Text></TouchableOpacity>}
         </View>
     )
 }
+
+const styles=StyleSheet.create({
+    container:{
+        flex:1,
+        flexDirection:'row',
+        paddingHorizontal:fontSize.sm,
+        justifyContent:'space-between',
+        marginVertical:8
+    },
+    title:{
+        fontSize:fontSize.base,
+        fontWeight:'500'
+    },
+    more:{
+        fontSize:fontSize.sm
+    },
+
+
+})
 
 export default Separator
